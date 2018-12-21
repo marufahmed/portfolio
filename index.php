817987af89ce8b5877723b3c7ea83786ce936e5e
Maruf <marufahmed@u.boisestate.edu>
2018-12-20 19:50:34 -0700

    Add index.php to redirect to index.html

---
+++ index.php
@@ -0,0 +1,11 @@
+function Redirect($url, $permanent = false)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+
+Redirect('index.html', false);
